8fdd6a576e2d5ba22055e557d6f6a4f82189b716
hwrnr <hawerner.hawerner@gmail.com>
2019-03-06 20:04:11 +0100

    Return password entry

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import os
 
 PORT = 8000
+passwordsDir = "passwords" + os.sep
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -17,7 +18,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         pattern = re.compile("\/.*\/$")
         if pattern.match(self.path):
             try:
-                files = os.listdir(self.path[1:] + "passwords/")
+                files = os.listdir(self.path[1:] + passwordsDir)
                 self.send_response(200)
                 self.send_header('Content-type', "text/html")
                 self.end_headers()
@@ -30,12 +31,26 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
                 self.send_error(404, "username not found")
                 self.end_headers()
                 return 
-        self.send_response(404)
-        self.send_header('Content-type', "text/html")
+        pattern = re.compile("\/.*\/.*$")
+        if pattern.match(self.path):
+            try:
+                username = self.path.split('/')[1]
+                password = self.path.split('/')[2]
+                if not os.path.isfile(username + os.sep + passwordsDir + password):
+                    self.send_error(404, "Password entry not found")
+                    return 
+                entry = open(username + os.sep + passwordsDir + password)
+                self.send_response(200)
+                self.send_header('Content-type', "text")
+                self.end_headers()
+                self.wfile.write(str.encode(entry.read()))
+                entry.close()
+                return 
+            except:
+                print("desio se except")
+                return
+        self.send_error(404, "Specify user with / at the end")
         self.end_headers()
-        f = open("index.html")
-        self.wfile.write(str.encode(f.read()))
-        f.close()
         return
 
 
@@ -46,5 +61,5 @@ with socketserver.TCPServer(("", PORT), handler) as httpd:
         print("Serving at port: " + str(PORT))
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print("Shutting down")
         httpd.socket.close()
+        print("Shutting down")

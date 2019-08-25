65a51bf22e1cce94e52cf4a64fc77e20da3d9e3d
hwrnr <hawerner.hawerner@gmail.com>
2019-08-25 18:25:56 +0200

    Added POST for password

--- flask-backend.py
+++ flask-backend.py
@@ -2,6 +2,7 @@ from flask import Flask, request, Response
 from flask_cors import CORS
 import json
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,7 @@ app.config['JSON_AS_ASCII'] = False
 
 PORT = 8000
 passwordsDir = "/passwords" + os.sep
+archiveDir = "/archive" + os.sep
 hashFileName = "/passhash"
 userDir = "user/"
 
@@ -47,15 +49,14 @@ def getSinglePassword(user, password):
         if passwordHash != passwordHashToCompare:
             return Response('{"error": "wrong password"}', status=403)
 
-        if request.method == 'GET':
-            username = userDir + user
-            #  password = "".join(['⁄' if word == "" else word for word in self.path.split('/')[3:]]) #not forward slash but an unicode char 2044 ⁄, used because forward slash is seperating files
-            if (password[-1] == '⁄'):
-                password = password[:-1]
-            password = password.replace("%20", " ")
+        username = userDir + user
+        #  password = "".join(['⁄' if word == "" else word for word in self.path.split('/')[3:]]) #not forward slash but an unicode char 2044 ⁄, used because forward slash is seperating files
+        if (password[-1] == '⁄'):
+            password = password[:-1]
+        password = password.replace("%20", " ")
 
+        if request.method == 'GET':
             if not os.path.isfile(username + os.sep + passwordsDir + password):
-                self.send_error(404, password.replace('⁄', '/') + " not found")
                 return Response('{"error": "password not found"}', status=404)
             entry = open(username + os.sep + passwordsDir + password)
             data = entry.read()
@@ -67,12 +68,26 @@ def getSinglePassword(user, password):
             temp['password'] = data.split('\n')[3]
             return json.dumps(temp)
 
+        if request.method == 'POST' or request.method == 'PATCH':
+            dataToWrite = request.args.to_dict()['entry'].to_dict()
+            if not ('usernameSalt' in dataToWrite and 'username' in dataToWrite and 'passwordSalt' in dataToWrite and 'password' in dataToWrite):
+                return Response('{"error": "missing some values"}', 400)
+            if os.path.isfile(username + os.sep + passwordsDir + password):
+                os.rename(username + os.sep + passwordsDir + password, username + os.sep + archiveDir + password + '  ' + str(datetime.now()))
+            f = open(username + os.sep + passwordsDir + password)
+            f.write(dataToWrite['usernameSalt'])
+            f.write(dataToWrite['username'])
+            f.write(dataToWrite['passwordSalt'])
+            f.write(dataToWrite['password'])
+            f.close()
+            return Response('{"success": true}', status=201)
+
         return Response('{"error": "not implemented"}', status=501)
     except Exception as ex:
         print('get single password')
         print(type(ex).__name__)
         print(ex)
-        return '{"error": 404}'
+        return Response('{"error": 404}', status=500)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=PORT)
